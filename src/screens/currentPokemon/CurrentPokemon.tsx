3ff161a075a7de2748efa4d23048617a1c38371c
Andrey <fail4iks@gmail.com>
2022-10-08 22:32:57 +0300

    Fix style

--- src/screens/currentPokemon/CurrentPokemon.tsx
+++ src/screens/currentPokemon/CurrentPokemon.tsx
@@ -1,8 +1,9 @@
-import {Text, View, Image} from "react-native";
+import {Text, View, Image, StyleSheet} from "react-native";
 import {CurrentsPokemonPropsType} from "../types";
 import {useEffect} from "react";
 import {useAppDispatch, useAppSelector} from "../../store/store";
 import {cleanCurrentPokemon, getCurrentPokemon} from "../../store/reducers/root";
+import {WIDTH} from "../../constants/constants";
 
 export const CurrentPokemon = ({route}: CurrentsPokemonPropsType) => {
     const {url} = route.params
@@ -17,13 +18,22 @@ export const CurrentPokemon = ({route}: CurrentsPokemonPropsType) => {
         }, []
     )
 
-    return <View>
-        <Text>All Pokemon</Text>
+    return <View style={styles.container}>
         {Object.keys(currentPokemon).length ? <View>
             <Image style={{width: 200, height: 200}}
                    source={{uri: currentPokemon.sprites.other['official-artwork'].front_default}}/>
-            <Text> {currentPokemon.name}
+            <Text style={styles.name}> {currentPokemon.name}
             </Text></View> : <View><Text>Loading...</Text>
         </View>}
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
+    name: {
+        paddingLeft: 50,
+        fontSize: 22
+    }
+})
